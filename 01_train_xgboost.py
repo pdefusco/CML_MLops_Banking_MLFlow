8a399f197e6ccae5edd0590fc21d626338c3a60c
pdefusco <pauldefusco@gmail.com>
2024-06-10 16:20:17 -0700

    env vars

--- 01_train_xgboost.py
+++ 01_train_xgboost.py
@@ -52,8 +52,8 @@ import pyspark.pandas as ps
 
 USERNAME = os.environ["PROJECT_OWNER"]
 DBNAME = "BNK_MLOPS_HOL_"+USERNAME
-STORAGE = "s3a://msc1-buk-f3156b92"
-CONNECTION_NAME = "msc1-aw-dl"
+STORAGE = "s3a://go01-demo"
+CONNECTION_NAME = "go01-aw-dl"
 
 DATE = date.today()
 EXPERIMENT_NAME = "xgb-cc-fraud-{0}".format(USERNAME)
