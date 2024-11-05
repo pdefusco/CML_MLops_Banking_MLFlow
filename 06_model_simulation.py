3df604b77d52e71dd83602736cec60517fbdda39
pdefusco <pauldefusco@gmail.com>
2024-11-04 16:21:03 -0800

    data connection

--- 06_model_simulation.py
+++ 06_model_simulation.py
@@ -59,7 +59,7 @@ import datetime
 # SET USER VARIABLES
 USERNAME = os.environ["PROJECT_OWNER"]
 DBNAME = "BNK_MLOPS_HOL_{}".format(USERNAME)
-CONNECTION_NAME = "ita-jul-aw-dl"
+CONNECTION_NAME = "paul-november-aw-dl"
 
 # Instantiate BankDataGen class
 dg = BankDataGen(USERNAME, DBNAME, CONNECTION_NAME)
