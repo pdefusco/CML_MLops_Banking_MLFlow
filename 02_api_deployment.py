9451f1c48b31290cbd7d21bb6926a603cb06a339
pdefusco <pauldefusco@gmail.com>
2026-05-14 13:47:52 -0700

    updated runtime value

--- 02_api_deployment.py
+++ 02_api_deployment.py
@@ -76,7 +76,7 @@ registeredModelResponse.model_versions[0].model_version_id
 createModelResponse = deployment.createModel(projectId, modelName, modelId)
 modelCreationId = createModelResponse.id
 
-runtimeId = "docker.repository.cloudera.com/cloudera/cdsw/ml-runtime-pbj-workbench-python3.10-standard:2025.09.1-b5" #Modify as needed
+runtimeId = "docker.repository.cloudera.com/cloudera/cdsw/ml-runtime-pbj-workbench-python3.10-standard:2026.04.1-b7" #Modify as needed
 createModelBuildResponse = deployment.createModelBuild(projectId, modelVersionId, modelCreationId, runtimeId)
 modelBuildId = createModelBuildResponse.id
 
